2aabdd371777f25713db2de4bde8364198d84325
lakton <lakton1337@gmail.com>
2023-05-24 13:19:00 +0300

    fail

---
+++ ckb.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x;
+    std::cout << x << std::endl; 
+
+    int arr[5];
+    arr[5] = 10; 
+
+    char buffer[10];
+    strcpy(buffer, "Hello, World!"); 
+
+    return 0;
+}
