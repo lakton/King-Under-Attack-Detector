c4b9a3a94a81e21d1af0bfdb2a9e5d33bc62e92f
lakton <98464538+lakton@users.noreply.github.com>
2023-05-25 17:15:30 +0300

    Update ChessMathTest.cpp

--- tests/ChessMathTest.cpp
+++ tests/ChessMathTest.cpp
@@ -1,4 +1,5 @@
-#include "King-Under-Attack-Detector/googletest-release-1.8.0/googletest/include/gtest/gtest.h"
+#include "gtest/gtest.h"
+
 #include "../ChessMath.h"
 
 TEST(ChessMathTest, IsWithinRangeTest) {
