7fe038dfe9d37992f7a7c96ff0c8ddcb8bf9a8c6
lakton <98464538+lakton@users.noreply.github.com>
2023-05-25 16:35:25 +0300

    Update cmake_install.cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lakton/Desktop/King-Under-Attack-Detector
+# Install script for directory: /home/runner/Desktop/King-Under-Attack-Detector
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lakton/Desktop/King-Under-Attack-Detector/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/runner/Desktop/King-Under-Attack-Detector/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
